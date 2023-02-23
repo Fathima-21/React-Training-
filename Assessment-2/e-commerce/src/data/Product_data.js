1009070c6b401a65d1b9f85ebc3b2c72810830a7
Fathima-21 <94165835+Fathima-21@users.noreply.github.com>
2023-02-23 09:35:00 +0530

    Add files via upload

---
+++ Assessment-2/e-commerce/src/data/Product_data.js
@@ -0,0 +1,25 @@
+const Product_data = [
+    {
+        "id":1,
+        "title": "Brown eggs", 
+        "type": "dairy",
+        "description": "Raw organic brown eggs in a basket",
+        "url": "https://via.placeholder.com/150x150.png?text=Product1",
+        "price": 28.1,
+        "rating": 4,
+        "image":"./images/1.jpg"
+
+
+    },
+    {
+        "id":2,
+        "title": "Sweet fresh stawberry",
+        "type": "fruit",
+        "description": "Sweet fresh stawberry on the wooden table",
+        "url": "https://via.placeholder.com/150x150.png?text=Product2",
+        "price": 29.45,
+        "rating": 4,
+        "image":"./images/2.jpg"
+    }
+]
+export default Product_data;
