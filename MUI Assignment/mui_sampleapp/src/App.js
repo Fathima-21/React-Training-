c27fcb91158934953b13df5899e1b0195a928034
Fathima-21 <94165835+Fathima-21@users.noreply.github.com>
2023-02-27 17:25:05 +0530

    Add files via upload

---
+++ MUI Assignment/mui_sampleapp/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Auth from './Auth';
+import Dashboard from './Dashboard';
+import Profile from './redux/Profile';
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Auth/>  */}
+     {/* <Dashboard/> */}
+     <Profile/>
+     
+    </div>
+  );
+}
+
+export default App;
